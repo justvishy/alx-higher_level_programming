65f2ee51bd85e4d5d4d8c48e0f834b772b79ae56
Vishnu <justvishy@gmail.com>
2024-08-14 09:11:18 +0000

    phw 8

--- 8-concat_edges.py
+++ 8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str.split("interactive, ")[1].split(" language")[0] + chr(10)
+str = str.split("interactive, ")[1].split(" language")[0] + str[-1]
 print(str)
