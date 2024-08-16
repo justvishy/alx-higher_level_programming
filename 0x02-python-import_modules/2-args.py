131f91a4dc27dd6c72058269ea8d252b322facd0
Vishnu <justvishy@gmail.com>
2024-08-16 11:37:18 +0000

    pim 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+f __name__ == "__main__":
+    from sys import argv
+
+    # Number of arguments
+    num_args = len(argv) - 1
+
+    # Print the number of arguments and the list of arguments
+    print("{:d} argument{}{}".format(
+        num_args, 's' if num_args != 1 else '', ':' if num_args > 0 else '.'))
+
+    # Print each argument and its position
+    for i in range(1, num_args + 1):
+        print("{:d}: {}".format(i, argv[i]))
